455bc1f16b2703d727042efa69e35e1c874bf004
Nathan <na.than.fjv@gmail.com>
2024-09-10 17:27:03 -0300

    feat: add first page

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import logo from "./assets/logo.svg";
+import letsStart from "./assets/lets-start-illustration.svg";
+import { Plus } from "lucide-react";
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="Logo LifeFlow" />
+      <img src={letsStart} alt="Start application with rocket and a woman" />
+
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        You haven't set any goals yet, how about setting one right now?
+      </p>
+
+      <Button>
+        <Plus className="size-4" />
+        Create Goal
+      </Button>
+    </div>
+  );
+}
